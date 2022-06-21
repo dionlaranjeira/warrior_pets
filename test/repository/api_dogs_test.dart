fdbd09d9e54f48203d2f6ab6ef1f23f18c27117c
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-06-21 11:58:25 -0400

    test: test apiDogs.getDogBreeds(page:int, limit:int)

--- test/repository/api_dogs_test.dart
+++ test/repository/api_dogs_test.dart
@@ -1,2 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:warrior_pets/repository/api_dogs.dart';
 
+void main() {
+
+  ApiDogs apiDogs = ApiDogs();
+
+  test("deve retornar um List<Map>", () async {
+    final List<Map> response = await apiDogs.getDogBreeds(0, 50);
+  });
+
+}
