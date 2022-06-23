bf70b24e88942ed0e5b7b8dfcc3cee0e7b4f4f37
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-06-23 13:21:27 -0400

    feat: added initial configs graphic others info cat

---
+++ lib/model/bar_chart.dart
@@ -0,0 +1,8 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+class BarChart{
+  String title;
+  int value;
+  final charts.Color color;
+
+  BarChart(this.title, this.value, this.color);
+}
