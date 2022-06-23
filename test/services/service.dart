58b107747a280e8a67990cdf6f975fd7b6e0e554
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-06-23 09:53:34 -0400

    test: fetchCatBreeds(page: int, limit:10) ok

--- test/services/service.dart
+++ test/services/service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:warrior_pets/model/cat_breed.dart';
 import 'package:warrior_pets/model/dog_breed.dart';
 import 'package:warrior_pets/services/service.dart';
 
@@ -8,6 +9,10 @@ void main() {
 
   test("deve retornar uma List<DogBreed>", () async {
     final List<DogBreed> response = await service.fetchDogBreeds(0, 50);
+  });
+
+  test("deve retornar uma List<CatBreed>", () async {
+    final List<CatBreed> response = await service.fetchCatBreeds(0, 50);
   });
 
 }
