9f43ffa738d88e2d1bf11c709f041fe945c2ff7f
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-06-23 09:57:06 -0400

    feat: added view/model ListCatsBreedsViewModel

---
+++ lib/view_model/list_cats_breeds.dart
@@ -0,0 +1,15 @@
+import 'package:warrior_pets/model/cat_breed.dart';
+import 'package:warrior_pets/services/service.dart';
+
+class ListCatsBreedsViewModel{
+  List<CatsBreedsViewModel>? catBreeds;
+  Future<void> fetchCatBreeds(int page, int limit) async {
+    final apiResult = await Service().fetchCatBreeds(page, limit);
+    catBreeds = apiResult.map((e) => CatsBreedsViewModel(e)).toList();
+  }
+}
+
+class CatsBreedsViewModel {
+  final CatBreed? catBreed;
+  CatsBreedsViewModel(this.catBreed);
+}
