2552cf2e2f17bda93b97d73147b8f223857f620c
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-06-24 20:51:13 -0400

    refactor: redesign details dogs

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,6 @@ void main() async {
         )
       ),
       debugShowCheckedModeBanner: false,
-      home: Login())
+      home: Home())
   );
 }
--- lib/views/dogs/detail_dog_breed.dart
+++ lib/views/dogs/detail_dog_breed.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:warrior_pets/util/colors_app.dart';
-import 'package:warrior_pets/util/utils.dart';
+
 class DetailDogBreed extends StatefulWidget {
   final dynamic dogBreed;
+
   const DetailDogBreed({Key? key, required this.dogBreed}) : super(key: key);
 
   @override
@@ -11,120 +11,151 @@ class DetailDogBreed extends StatefulWidget {
 }
 
 class _DetailDogBreedState extends State<DetailDogBreed> {
+  String urlPhotoNull =
+      "https://www.pngall.com/wp-content/uploads/10/Pet-Silhouette.png";
 
-
-  String urlPhotoNull = "https://www.pngall.com/wp-content/uploads/10/Pet-Silhouette.png";
   @override
   Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(
-        title: Text(widget.dogBreed.name),
+        backgroundColor: ColorsApp.secundarybackground,
+        title: textAppBar(context),
+        automaticallyImplyLeading: false,
       ),
       body: MediaQuery(
-      data: const MediaQueryData(),
-      child: Scaffold(
-        backgroundColor: ColorsApp.secundarybackground,
-        body: Column(
-          children: [
-            Stack(
-              clipBehavior: Clip.none,
+        data: const MediaQueryData(),
+        child: Scaffold(
+          backgroundColor: ColorsApp.secundarybackground,
+          body: SingleChildScrollView(
+            child: Column(
               children: [
-                Container(
-                    child: Image.network(widget.dogBreed.image!.url ?? urlPhotoNull),
-                ),
-                Positioned(
-                  bottom: -50,
-                  left: 0,
-                  right: 0,
-                  child: Center(
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      constraints: const BoxConstraints(maxWidth: 400),
-                      height: 100,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        boxShadow: [
-                          BoxShadow(color: ColorsApp.background, spreadRadius: 4),
-                        ],
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Image.network(widget.dogBreed.image!.url ?? urlPhotoNull),
+                    Positioned(
+                      bottom: -50,
+                      left: 0,
+                      right: 0,
+                      child: Center(
+                        child: containerInforsDog(context),
                       ),
-                    child: Column(
-                      children: [
-                        Text("Dog Category: "+widget.dogBreed.breedGroupString,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontWeight: FontWeight.bold, color: ColorsApp.primaryColor, fontSize: 18),),
-                        Text(
-                          widget.dogBreed.bredForString,
-                          style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 90),
+                paddingExtraInforsDogs(),
+                const SizedBox(height: 20),
+                btnBack(context),
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ));
+
+  ElevatedButton btnBack(BuildContext context) {
+    return ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Back"),
+                style: ElevatedButton.styleFrom(
+                    primary: ColorsApp.secondaryColor,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 10),
+                    textStyle: const TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.bold)),
+              );
+  }
+
+  Padding paddingExtraInforsDogs() {
+    return Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16),
+                child: Text(
+                  "Temperament: " + widget.dogBreed.temperament,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: ColorsApp.primaryColor,
+                    fontSize: 18,
+                  ),
+                ),
+              );
+  }
+
+  Container containerInforsDog(BuildContext context) {
+    return Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(32)),
+                          boxShadow: [
+                            BoxShadow(
+                                color: ColorsApp.background, spreadRadius: 4),
+                          ],
                         ),
-                        
-                        
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                        child: Column(
                           children: [
-                            const Icon(Icons.pin_drop_outlined),
-                            const SizedBox(width: 10,),
-                            Text(widget.dogBreed.originString, textAlign: TextAlign.center,
-                              style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                            Text(
+                              "Dog Category: " +
+                                  widget.dogBreed.breedGroupString,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: ColorsApp.primaryColor,
+                                  fontSize: 14),
+                            ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child:
+                            Text(
+                                widget.dogBreed.bredForString,
+                                textAlign: TextAlign.center)
+                          ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.pin_drop_outlined, size: 14),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  widget.dogBreed.originString,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.6)),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(Icons.calendar_today,size: 14),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  widget.dogBreed.lifeSpan,
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.6)),
+                                ),
+                              ],
                             ),
                           ],
                         ),
+                      );
+  }
 
-                      ],
-                    ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 150),
-            const Text(
-              'Compre produtos indígenas',
-              style: TextStyle(
-                color: ColorsApp.primaryColor,
-                fontSize: 22,
-              ),
-            ),
-
-            const SizedBox(height: 80,),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                    width: 15.0,
-                    height: 15.0,
-                    decoration: const BoxDecoration(
-                      color: ColorsApp.primaryColor,
-                      shape: BoxShape.circle,
-                    )),
-                const SizedBox(width: 15,),
-                Container(
-                    width: 15.0,
-                    height: 15.0,
-                    decoration: const BoxDecoration(
-                      color: ColorsApp.primaryColor,
-                      shape: BoxShape.circle,
-                    )),
-                const SizedBox(width: 15,),
-                Container(
-                    width: 15.0,
-                    height: 15.0,
-                    decoration: const BoxDecoration(
-                      color: ColorsApp.primaryColor,
-                      shape: BoxShape.circle,
-                    )),
-              ],
-            ),
-
-            Expanded(child: Container()),
-          ],
-        ),
-      ),
-    )
-
-
-
-
-
-
-  );
+  Container textAppBar(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(16.0),
+        width: MediaQuery.of(context).size.width * 0.8,
+        child:
+            Text(
+                widget.dogBreed.name,
+                textAlign: TextAlign.center,style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: ColorsApp.primaryColor)),
+      );
+  }
 }
