650360e82c14014f9b06e90d860775232233b27e
dionlaranjeira <dionribeiro.rr@gmail.com>
2022-06-25 15:21:36 -0400

    refactor: redesign details cats

--- lib/views/cats/detail_cat_breed.dart
+++ lib/views/cats/detail_cat_breed.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:warrior_pets/util/utils.dart';
+import 'package:warrior_pets/util/colors_app.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:warrior_pets/model/bar_chart.dart';
 
 class DetailCat extends StatefulWidget {
   final dynamic catBreed;
+
   const DetailCat({Key? key, required this.catBreed}) : super(key: key);
 
   @override
@@ -13,91 +13,173 @@ class DetailCat extends StatefulWidget {
 }
 
 class _DetailCatState extends State<DetailCat> {
-  String urlPhotoNull = "https://www.creativefabrica.com/wp-content/uploads/2021/01/26/Cat-Icon-Graphics-8071439-1.jpg";
+  String urlPhotoNull =
+      "https://www.creativefabrica.com/wp-content/uploads/2021/01/26/Cat-Icon-Graphics-8071439-1.jpg";
 
   @override
   Widget build(BuildContext context) {
-
     final List<BarChart> data = [
-      BarChart("Affection", widget.catBreed.affectionLevel, charts.ColorUtil.fromDartColor(Colors.red)),
-      BarChart("Energy", widget.catBreed.energyLevel, charts.ColorUtil.fromDartColor(Colors.yellow)),
-      BarChart("Grooming", widget.catBreed.grooming, charts.ColorUtil.fromDartColor(Colors.green)),
-      BarChart("Intelligence", widget.catBreed.intelligence, charts.ColorUtil.fromDartColor(Colors.purple)),
+      BarChart("Affection", widget.catBreed.affectionLevel,
+          charts.ColorUtil.fromDartColor(Colors.red)),
+      BarChart("Energy", widget.catBreed.energyLevel,
+          charts.ColorUtil.fromDartColor(Colors.yellow)),
+      BarChart("Grooming", widget.catBreed.grooming,
+          charts.ColorUtil.fromDartColor(Colors.green)),
+      BarChart("Intelligence", widget.catBreed.intelligence,
+          charts.ColorUtil.fromDartColor(Colors.purple)),
     ];
 
     List<charts.Series<BarChart, String>> series = [
       charts.Series(
         id: widget.catBreed.id,
         data: data,
-        domainFn: (BarChart series, _)=>series.title,
-        measureFn: (BarChart series, _)=>series.value,
-        colorFn: (BarChart series, _)=>series.color,
+        domainFn: (BarChart series, _) => series.title,
+        measureFn: (BarChart series, _) => series.value,
+        colorFn: (BarChart series, _) => series.color,
       ),
     ];
-    return
-  Scaffold(
-    appBar: AppBar(
-      title: Text(widget.catBreed.name),
-    ),
-    body:   SingleChildScrollView(
-      child:
-      Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            ListTile(
-              leading: SvgPicture.asset(Utils.urlIconCat, width: 50),
-              title: Text("Origin: "+widget.catBreed.origin),
-              subtitle: Text(
-                widget.catBreed.temperament,
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              ),
-            ),
-            Hero(
-              tag: widget.catBreed.id!,
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  child: Image.network(widget.catBreed.image!.url ?? urlPhotoNull),
-                ),
-              ),
-            ),
-            Container(
-              height: 160,
-              padding: const EdgeInsets.only(bottom: 16),
-              child: charts.BarChart(series, animate: true),),
 
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(
-                widget.catBreed.description +"\nLife Span: " + widget.catBreed.lifeSpan, textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              ),
-            )
-
-
-          ],
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: ColorsApp.secundarybackground,
+          title: textAppBar(context),
+          automaticallyImplyLeading: false,
         ),
+        body: MediaQuery(
+          data: const MediaQueryData(),
+          child: Scaffold(
+            backgroundColor: ColorsApp.secundarybackground,
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      Image.network(widget.catBreed.image!.url ?? urlPhotoNull),
+                      Positioned(
+                        bottom: -50,
+                        left: 0,
+                        right: 0,
+                        child: Center(
+                          child: containerInforsCat(context),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 90),
+                  graphicInforCat(series),
+                  paddingExtraInforsCats(),
+                  const SizedBox(height: 20),
+                  btnBack(context),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+
+  Container textAppBar(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: Text(widget.catBreed.name,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              fontWeight: FontWeight.bold, color: ColorsApp.primaryColor)),
+    );
+  }
+
+  Container containerInforsCat(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(32)),
+        boxShadow: [
+          BoxShadow(color: ColorsApp.background, spreadRadius: 4),
+        ],
       ),
-    ),
-    bottomNavigationBar:   ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          primary: Colors.blue,
-          onPrimary: Colors.white,
-          shadowColor: Colors.blueAccent,
-          elevation: 3,
-        ),
-        onPressed: (){Navigator.pop(context);},
-        child:  Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
+      child: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Text(
+              widget.catBreed.temperament,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: ColorsApp.primaryColor,
+                  fontSize: 14),
+            ),
+          ),
+
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Icon(Icons.arrow_back_ios),
-              SizedBox(width: 10),
-              Text("Back", style: TextStyle(fontSize: 18)),
+            children: [
+              const Icon(Icons.pin_drop_outlined, size: 14),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                widget.catBreed.origin,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black.withOpacity(0.6)),
+              ),
             ],
           ),
-        )),
-  );}
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.calendar_today, size: 14),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                widget.catBreed.lifeSpan + " years",
+                style: TextStyle(color: Colors.black.withOpacity(0.6)),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container graphicInforCat(List<charts.Series<BarChart, String>> series) {
+    return Container(
+        height: 160,
+        padding: const EdgeInsets.only(bottom: 16),
+        child: charts.BarChart(series, animate: true));
+  }
+
+  Padding paddingExtraInforsCats() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Text(
+        widget.catBreed.description,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          color: ColorsApp.primaryColor,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  ElevatedButton btnBack(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      child: const Text("Back"),
+      style: ElevatedButton.styleFrom(
+          primary: ColorsApp.secondaryColor,
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+          textStyle:
+          const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+    );
+  }
+
 }
